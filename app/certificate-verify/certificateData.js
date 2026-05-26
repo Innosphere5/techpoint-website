190c5efc1863c9a95f7f621a9eb1e12c5e77815d
Innosphere5 <innosphere.digital5@gmail.com>
2026-05-26 13:49:19 +0530

    new commit

--- app/certificate-verify/certificateData.js
+++ app/certificate-verify/certificateData.js
@@ -16,8 +16,8 @@ export const certificateDatabase = {
     pdfPath: "/certificates/simranjeet kaur.pdf",
   },
    859: {
-    name: "Neev Ghai",
-    pdfPath: "/certificates/NEEV DIPLOMA 1 YEAR (1).pdf",
+    name: "Neev",
+    pdfPath: "/certificates/NEEV.pdf",
   },
     404: {
     name: "Pushpinder kaur",
